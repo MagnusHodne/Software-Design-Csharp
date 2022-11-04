b57a15deb278f6fb0c16c5684eab797c3639e4a6
Magnus Hodne <magnus@drobak.org>
2022-11-04 14:10:29 +0100

    Created logger class

---
+++ MultiThreadedConsoleApp/Logger.cs
@@ -0,0 +1,31 @@
+﻿namespace MultiThreadedConsoleApp;
+
+public class Logger
+{
+    private string _ownerClassName;
+    public Logger(string ownerClassName) {
+        _ownerClassName = ownerClassName;
+    }
+    public void Info(string message) {
+        Log(message, "INFO", ConsoleColor.Blue, _ownerClassName);
+    }
+
+    public void Debug(string message) {
+        Log(message, "INFO", ConsoleColor.Magenta, _ownerClassName);
+    }
+
+    public void Error(string message) {
+        Log(message, "INFO", ConsoleColor.Red, _ownerClassName);
+    }
+
+    public void Warn(string message) {
+        Log(message, "INFO", ConsoleColor.Yellow, _ownerClassName);
+    }
+    
+    private static void Log(string message, string type, ConsoleColor color, string className) {
+        var prevColor = Console.ForegroundColor;
+        Console.ForegroundColor = color;
+        Console.WriteLine($"[{type}] {className}: {message}");
+        Console.ForegroundColor = prevColor;
+    } 
+}
